c9c465664231b10d5ffb37e3a842fa2b9a55b656
Jordan Hudgens <jordan@devcamp.com>
2017-07-04 19:31:45 +0000

    Added nav bar and added default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+  
+  before_action :set_title
+  
+  def set_title
+    @page_title = "My Portfolio Website"
+  end
 end
