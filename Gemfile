0aa43feb6a8052e338795c38b078b8f370572e9a
Jordan Hudgens <jordan@devcamp.com>
2017-07-18 17:02:46 +0000

    updated kaylie view tool

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
+
+gem "kaylie_view_tool", git: "https://github.com/KaylieHollander/Kaylie_view_tool"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,3 @@ class ApplicationController < ActionController::Base
   
 end
 
-module DevcampViewTool
-  class Renderer
-    def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
-end
